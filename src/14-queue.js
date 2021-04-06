a6b311baec6acd7c45ce8deda05bb719966e8bb6
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 23:46:50 +0300

    feat: 14 -test passed

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,11 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode1 = require('../extensions/list-node');
+
+ListNode1(5);
+
+function ListNode(x) {
+  this.value = x;
+  this.next = null;
+}
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,16 +19,47 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.value = new ListNode();
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    let val = this.value;
+    const arr2 = [val];
+    while (val.next) {
+      val = val.next;
+      arr2.push(val);
+    }
+    return arr2.length;
+  }
+
+  enqueue(element) {
+    if (typeof this.value.value === 'undefined') {
+      this.value.value = element;
+    } else {
+      let val = this.value;
+      const arr2 = [val];
+      while (val.next) {
+        val = val.next;
+        arr2.push(val);
+      }
+      arr2.push(new ListNode(element));
+      let i = arr2.length;
+      while (i > 1) {
+        arr2[i - 2].next = arr2[i - 1];
+        i--;
+      }
+      [this.value] = [arr2[0]];
+    }
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const arr2 = [this.value];
+    if (this.value.next) {
+      arr2.push(this.value.next);
+    }
+    [this.value] = [arr2[1]];
+    return arr2[0].value;
   }
 }
 
