39f288e48e5fe125b361eb19cf95a12c532abd09
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 17:22:03 +0300

    feat: 10 -test passed

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,18 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let curr = [0, Math.round(array.length / 2), array.length];
+  let i = 0;
+  while (array[curr[1]] !== value && i < 1000) {
+    if (array[curr[1]] > value) {
+      curr = [curr[0], Math.floor((curr[0] + curr[1]) / 2), curr[1]];
+    } else {
+      curr = [curr[1], Math.round((curr[2] + curr[1]) / 2), curr[2]];
+    }
+    i++;
+  }
+  return curr[1];
 }
 
 module.exports = findIndex;
