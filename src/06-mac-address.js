516e4954775d7f0aaa4de10ff648d6675a82399b
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 14:29:27 +0300

    feat: 06 -test passed

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,30 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  if (n.length !== 17) {
+    return false;
+  }
+  if (n[2] !== '-' || n[5] !== '-' || n[8] !== '-' || n[11] !== '-' || n[14] !== '-') {
+    return false;
+  }
+  const str = '09AF';
+  for (let i = 0; i < n.length; i++) {
+    if (i !== 2 && i !== 5 && i !== 8 && i !== 11 && i !== 14) {
+      if (
+        n.charCodeAt(i) < str.charCodeAt(0) || n.charCodeAt(i) > str.charCodeAt(3)
+      ) {
+        return false;
+      }
+      if (
+        n.charCodeAt(i) > str.charCodeAt(1)
+        && n.charCodeAt(i) < str.charCodeAt(2)
+      ) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = isMAC48Address;
