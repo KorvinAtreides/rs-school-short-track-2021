febf90eb778bff3356e86ae5ed3c05255c2ef476
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 17:44:07 +0300

    feat: 11 -test passed

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const arr2 = [];
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (String(arr[i]) === String(arr[i + 1])) {
+      count++;
+    } else {
+      if (count === 1) {
+        arr2.push(arr[i]);
+      } else {
+        arr2.push(String(count) + arr[i]);
+      }
+      count = 1;
+    }
+  }
+  return arr2.join('');
 }
 
 module.exports = encodeLine;
