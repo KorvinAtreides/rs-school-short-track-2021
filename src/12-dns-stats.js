6fcee7b725129853abf4d765c527998b2f09728b
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 20:20:29 +0300

    feat: 12 -test passed

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,25 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    const arr = domains[i].split('.').reverse();
+    arr.forEach((el, k) => {
+      arr[k] = String('.') + String(el);
+    });
+    for (let l = 1; l < arr.length; l++) {
+      arr[l] = String(arr[l - 1]) + String(arr[l]);
+    }
+    for (let j = 0; j < arr.length; j++) {
+      if (obj[arr[j]]) {
+        obj[arr[j]] += 1;
+      } else {
+        obj[arr[j]] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
