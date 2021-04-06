865a3e86d81e6133889bcd345aa195d1313190c1
KorvinAtreides <Korvin1312b@yandex.by>
2021-04-06 13:40:17 +0300

    feat: 04 -test passed

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,51 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const matr = new Array(matrix.length);
+  for (let i = 0; i < matrix.length; i++) {
+    matr[i] = new Array(matrix[i].length);
+  }
+  for (let i = 0; i < matr.length; i++) {
+    for (let j = 0; j < matr[i].length; j++) {
+      matr[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1]) {
+          matr[i][j]++;
+        }
+        if (matrix[i - 1][j]) {
+          matr[i][j]++;
+        }
+        if (matrix[i - 1][j + 1]) {
+          matr[i][j]++;
+        }
+      }
+
+      if (matrix[i][j - 1]) {
+        matr[i][j]++;
+      }
+      if (matrix[i][j + 1]) {
+        matr[i][j]++;
+      }
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1]) {
+          matr[i][j]++;
+        }
+        if (matrix[i + 1][j]) {
+          matr[i][j]++;
+        }
+        if (matrix[i + 1][j + 1]) {
+          matr[i][j]++;
+        }
+      }
+    }
+  }
+
+  return matr;
 }
 
 module.exports = minesweeper;
